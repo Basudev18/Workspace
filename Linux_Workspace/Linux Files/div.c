f2a1409806f0f18d5d462ada98cc07bb2105900b
Basudev Das <91820244+Basudev18@users.noreply.github.com>
2023-03-20 12:35:01 +0530

    Add files via upload

---
+++ Linux_Workspace/Linux Files/div.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+float div(int a,int b)
+{
+        return a/b;
+}
+
---
+++ Linux_Workspace/Linux Files/value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello");
+	int a =10;
+	int b =20;
+	int c;
+	c =a+b;
+	printf("value of c:%d\n",c);
+	return 0;
+}
