b4ec19bc076d28bf17182a2cd6b731da36d038dd
Basudev18 <das1basudev1995@gmail.com>
2023-03-21 08:33:22 +0530

    Linux files
    Signed-off-by: Basudev18 <das1basudev1995@gmail.com>

---
+++ Linux_Workspace/Day1/Day2/Q1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int X,M,L;
+	scanf("%d %d %d,&X,&M,&L")
+	printf("The Reverse of XML is LMX:\n");
+	scanf("%c");
+	return 0;
+}
---
+++ Linux_Workspace/Linux Files/Operators.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    unsigned double(d=15.5)	
+    unsigned float(f=12.2)
+    printf("%lf\n",d);
+    printf("%f\n",f);
+    return 0;
+}
